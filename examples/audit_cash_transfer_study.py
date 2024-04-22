ec6ee9b186da7f59ec127a61cddc3c6a8b9f1ee5
Ittai Rubinstein <ittai.rubinstein@gmail.com>
2024-04-22 18:53:13 -0400

    Converted martinez to run in the same format as other examples and made the ohie analysis try to load the hectacres parameter in log-scale as well.

--- examples/audit_cash_transfer_study.py
+++ examples/audit_cash_transfer_study.py
@@ -5,26 +5,30 @@ from pathlib import Path
 import os
 from data.angelucci_degiorgi_data import load_angelucci_data
 
-datasets = load_angelucci_data(1)
+
 
 CURRENT_DIR = Path(__file__).resolve().parent
 base_dir = CURRENT_DIR / "results" / "cash_transfers"
 results = []
-for dataset in datasets:
-    name = f"t_{dataset.time_period}_treat_{dataset.treatment}"
-    for i in range(2):
-        print('*'*80)
-    print(f"Running on {name=}")
-    for i in range(2):
-        print('*'*80)
-    output_dir = base_dir / name
-    config = AuditorConfig(output_dir=output_dir)
-    ra = RobustnessAuditor(dataset.regression, config)
-    ra.compute_all_bounds(categorical_aware=True)
-    ra.plot_removal_effects()
-    result = ra.summary()
-    result["experiment"] = name
-    results.append(result)
-    df = pd.DataFrame(results)
-    df = df[["experiment"] + [col for col in df.columns if col != "experiment"]]
-    print(df)
+for log_hectareas in [True, False]:
+    datasets = load_angelucci_data(which_regression=1, log_hectareas=log_hectareas)
+    for dataset in datasets:
+        name = f"t_{dataset.time_period}_treat_{dataset.treatment}_log_hectareas_{log_hectareas}"
+        for i in range(2):
+            print('*'*80)
+        print(f"Running on {name=}")
+        for i in range(2):
+            print('*'*80)
+        output_dir = base_dir / name
+        config = AuditorConfig(output_dir=output_dir)
+        ra = RobustnessAuditor(dataset.regression, config)
+        ra.compute_all_bounds(categorical_aware=True)
+        ra.plot_removal_effects()
+        result = ra.summary()
+        result["experiment"] = name
+        results.append(result)
+        df = pd.DataFrame(results)
+        df = df[["experiment"] + [col for col in df.columns if col != "experiment"]]
+        print(df)
+
+        df.to_csv(base_dir / "results.csv")
